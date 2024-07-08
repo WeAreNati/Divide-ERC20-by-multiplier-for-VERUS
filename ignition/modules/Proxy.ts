0b267cbffb83abc802a2bc185050c097ed261304
Chris <sid.Cartwight1982@protonmail.com>
2024-07-08 15:53:22 +0100

    Have changeable address

--- ignition/modules/Proxy.ts
+++ ignition/modules/Proxy.ts
@@ -4,11 +4,10 @@ const TOKEN_NAME = "GNATI"
 const TOKEN_TICKER = "NOT"
 const LINKED_CONTRACT ="0xe73b92e469c49a4651aeff204ecfe920a78022db" //use the real ERC20 that will be reduced.
 const IADDRESS = "0xffEce948b8A38bBcC813411D2597f7f8485a0689"   // convert the iaddress of the Verus token mapped to to  20 bytes
-const VERUSBRIDGE = "0xcaa98a4ec79dac8a06cb3bfdcf5351b6576d939f" // The verus bridge contract delegator address.
 
 const p = buildModule("Proxy", (m) => {
 
-  const proxy = m.contract("GNATI_BRIDGE", [TOKEN_NAME, TOKEN_TICKER, LINKED_CONTRACT, IADDRESS, VERUSBRIDGE], {});
+  const proxy = m.contract("GNATI_BRIDGE", [TOKEN_NAME, TOKEN_TICKER, LINKED_CONTRACT, IADDRESS], {});
 
   return { proxy };
 });
